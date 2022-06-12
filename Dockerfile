2f8c25745ebaae62e7c54454daa2c0ae3850bb14
haind-2716 <nguyen.duc.hai@sun-asterisk.com>
2022-06-12 10:53:58 +0700

    init project

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# syntax=docker/dockerfile:1
+
+FROM golang:1.17 as base
+
+# for develop
+FROM base as dev
+
+# install the air binary so we get live code-reloading when we save files
+RUN curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
+
+# run air command in the directory where our code will live
+WORKDIR /hackernew-scrap
+
+CMD ["air"]
---
+++ configs/database.go
@@ -0,0 +1,23 @@
+package configs
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+type DBConfig struct {
+	Connection string `envconfig:"DB_CONNECTION" default:"postgres"`
+	Host       string `envconfig:"DB_HOST" default:"localhost"`
+	Port       string `envconfig:"DB_PORT" default:"5432"`
+	User       string `envconfig:"DB_USER" default:"postgres"`
+	Password   string `envconfig:"DB_PASSWORD" default:"postgres"`
+	DBName     string `envconfig:"DB_DBNAME" default:"postgres"`
+	SSLMode    string `envconfig:"DB_SSLMODE" default:"disable"`
+}
+
+func NewDBConfig() (DBConfig, error) {
+	var dbconfig DBConfig
+
+	err := envconfig.Process("", &dbconfig)
+
+	return dbconfig, err
+}
