26a6167ab2e633b684666d30693c99b4bc8b1480
haind-2716 <nguyen.duc.hai@sun-asterisk.com>
2022-06-15 13:58:56 +0700

    init usecase

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	newsRepository "hackernew-scrap/domain/scrap/repository"
+	newsUsecase "hackernew-scrap/domain/scrap/usecase"
 	"hackernew-scrap/infrastructure"
 )
 
 func main() {
+
+	newsRepository := newsRepository.NewRepsitory(infrastructure.DB)
+	_ = newsUsecase.NewNewsUsecase(newsRepository)
+
 	infrastructure.InfoLog.Println("run server")
+
 }
